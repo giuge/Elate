125c61b80e5179f21bf248acfbe872e2ddc9105d
Giuseppe Germinaro <giuge@germinaro.gs>
2016-02-23 13:07:07 +0100

    Minor amendments

--- app/lib/env.js
+++ app/lib/env.js
@@ -1,15 +1,14 @@
 // Simple module exposes environment variables to rest of the code.
-
 import jetpack from 'fs-jetpack'
 
 var app
 if (process.type === 'renderer') {
-    app = require('electron').remote.app
+  app = require('electron').remote.app
 } else {
-    app = require('electron').app
+  app = require('electron').app
 }
-var appDir = jetpack.cwd(app.getAppPath())
 
+var appDir = jetpack.cwd(app.getAppPath())
 var manifest = appDir.read('package.json', 'json')
 
 export default manifest.env
