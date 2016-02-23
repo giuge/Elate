14f2acffa197cfda74b5b637da4222b177a19c97
Giuseppe Germinaro <giuge@germinaro.gs>
2016-02-23 11:36:06 +0100

    😎  Git should not ignore how I name ma filez

---
+++ app/components/spinner.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+export default class Spinner extends Component {
+  render () {
+    return (
+      <div className="ispinner white large animating">
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+        <div className="ispinner-blade"></div>
+      </div>
+    )
+  }
+}
---
+++ app/components/topbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import remote from 'remote'
+
+
+export default class TopBar extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleDoubleClick() {
+    let currentWindow = remote.getCurrentWindow()
+    let bounds = currentWindow.getBounds()
+    if(bounds.width !== screen.width && bounds.height !== screen.height) {
+      currentWindow.maximize()
+    } else {
+      currentWindow.unmaximize()
+    }
+  }
+
+  render () {
+    return (
+      <div className='topBar' onDoubleClick={() => { this.handleDoubleClick() }}>
+      </div>
+    )
+  }
+}
