91c69f19b3f7e1c04d7c4d077e686dbe3343bdad
Giuseppe Germinaro <giuge@germinaro.gs>
2016-02-26 13:11:19 +0100

    Using lowdb

--- app/lib/constants.js
+++ app/lib/constants.js
@@ -23,7 +23,7 @@ export const CONTENT_ROOT = 'https://content.dropboxapi.com/2'
 export const APP_KEY = 'mn8nb1bz34sxpe3'
 export const APP_SECRET = '0lpmbxuiiy4qe42'
 export const OAUTH_REDIRECT_URL = 'http://localhost/oauth'
-export const MEDIA_FOLDER = '/Camera Uploads'
+export const MEDIA_FOLDER = '/test'
 
 // Geocoder
 export const GEOCODER_API_KEY = 'AthmNCikQpDVlLZZ_8QV4nNJaLq7bLnolOPnQgd5pJK23nyV0WT6FX7XFiUSPxQI'
